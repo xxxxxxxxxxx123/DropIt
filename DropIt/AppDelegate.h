1deff835615d23408644f9536886b1891e5a0959
john shen <johnshencanada@gmail.com>
2014-06-21 17:10:04 -0700

    first commit

---
+++ DropIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DropIt
+//
+//  Created by john on 6/21/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DropIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DropIt
+//
+//  Created by john on 6/21/14.
+//  Copyright (c) 2014 Banana Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
